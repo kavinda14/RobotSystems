a1d7a1e01d282ea2e620493ebf2f695355d9a2b2
Kavinda Senewiratne <senewiry@oregonstate.edu>
2022-03-03 19:06:05 -0800

    modified

--- Functions/week7.py
+++ Functions/week7.py
@@ -1,118 +1,236 @@
-from ColorTracking import *
+from HiwonderSDK.Board import Board
+from CameraCalibration.CalibrationConfig import *
+from ArmIK.ArmMoveIK import *
+from ArmIK.Transform import *
+from LABConfig import *
+import threading
+import Camera
+import time
+import cv2
+import sys
 
-class Perception:
-    def __init__(self, img):
-        global roi
-        global rect
-        global count
-        global track
-        global get_roi
-        global center_list
-        global __isRunning
-        global unreachable
-        global detect_color
-        global action_finish
-        global rotation_angle
-        global last_x, last_y
-        global world_X, world_Y
-        global world_x, world_y
-        global start_count_t1, t1
-        global start_pick_up, first_move
+sys.path.append("/home/pi/ArmPi")
 
-        self.img = img
 
-    def _modify_img(self):
-        img_copy = self.img.copy()
-        img_h, img_w = self.img.shape[:2]
-        cv2.line(self.img, (0, int(img_h / 2)), (img_w, int(img_h / 2)), (0, 0, 200), 1)
-        cv2.line(self.img, (int(img_w / 2), 0), (int(img_w / 2), img_h), (0, 0, 200), 1)
+AK = ArmIK()
 
-        if not __isRunning:
-            return self.img
+range_rgb = {
+    "red": (0, 0, 255),
+    "blue": (255, 0, 0),
+    "green": (0, 255, 0),
+    "black": (0, 0, 0),
+    "white": (255, 255, 255),
+}
 
-        frame_resize = cv2.resize(img_copy, size, interpolation=cv2.INTER_NEAREST)
-        frame_gb = cv2.GaussianBlur(frame_resize, (11, 11), 11)
-        #如果检测到某个区域有识别到的物体，则一直检测该区域直到没有为止
-        # If a region is detected with a recognized object, the region is detected until it is not
-        if get_roi and start_pick_up:
-            get_roi = False
-            frame_gb = getMaskROI(frame_gb, roi, size)
 
-        # 将图像转换到LAB空间 Convert images to LAB space
-        frame_lab = cv2.cvtColor(frame_gb, cv2.COLOR_BGR2LAB)
+class BoxDetector:
+    def __init__(self):
+        """
+        TODO: there's all sorts of parameters in the various
+        class methods that could (should) be accesssible on init
+        """
+        self.camera = Camera.Camera()
+        self.camera.camera_open()
 
-        return frame_lab
+    def __del__(self):
+        # Cleanup camera on delete
+        self.camera.camera_close()
 
-    def _process_contours(self):
-        detect_color = i
-        # 对原图像和掩模进行位运算 Bitwise operation of the original image and mask
-        frame_mask = cv2.inRange(
-            frame_lab, color_range[detect_color][0], color_range[detect_color][1])
-        opened = cv2.morphologyEx(frame_mask, cv2.MORPH_OPEN, np.ones(
-            (6, 6), np.uint8))  # 开运算 Open computing
-        closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, np.ones(
-            (6, 6), np.uint8))  # 闭运算 Closed operations
-        contours = cv2.findContours(
-            closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]  # 找出轮廓 Find the outline
-        areaMaxContour, area_max = getAreaMaxContour(
-            contours)  # 找出最大轮廓 Find the maximum profile
+    def get_max_contour(self, mask, min_area=300):
+        """
+        Perform filtering for contour detection: open, close
+        Then find the largest contour by area
+        Input a mask of the desired color(s)
+        Return the contour and its area, None if invalid
+        """
+        max_contour = None
 
-        return areaMaxContour, area_max
-    
-    def process_img(self):
-        frame_lab = self._modify_img()
+        # Filtering
+        opened = cv2.morphologyEx(
+            mask, cv2.MORPH_OPEN, np.ones((6, 6), np.uint8))
+        closed = cv2.morphologyEx(
+            opened, cv2.MORPH_CLOSE, np.ones((6, 6), np.uint8))
+        contours = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[
+            -2
+        ]
+
+        # Calculate area for each contour, find largest area
+        def area(contour):
+            return math.fabs(cv2.contourArea(contour))
+
+        areas = list(map(area, contours))
+        max_area = max(areas)
+
+        # Only return contour if max > min
+        if max_area > min_area:
+            max_contour = contours(areas.index(max_area))
+
+        return max_contour, max_area
+
+    def get_color_contour(self, img_lab, target_colors):
+        """
+        Find the largest contour by area over all targeted colors,
+        returns that contour, its color, and its area
+        """
+        largest_contour = None
+        detected_color = None
         area_max = 0
-        areaMaxContour = 0
-        if not start_pick_up:
-            for i in color_range:
-                if i in __target_color:
-                    areaMaxContour, area_max = self._process_contours()
-            if area_max > 2500:  # 有找到最大面积 Have found the maximum area
-                rect = cv2.minAreaRect(areaMaxContour)
-                box = np.int0(cv2.boxPoints(rect))
 
-                roi = getROI(box)  # 获取roi区域 Get roi area
-                get_roi = True
+        for i in color_range:  # color_range from LABConfig
+            if i in target_colors:
+                # Mask for this color in image
+                color_mask = cv2.inRange(
+                    img_lab, color_range[detect_color][0], color_range[detect_color][1]
+                )
 
-                # 获取木块中心坐标 Get the coordinates of the center of the block
-                img_centerx, img_centery = getCenter(
-                    rect, roi, size, square_length)
-                # 转换为现实世界坐标 Convert to real-world coordinates
-                world_x, world_y = convertCoordinate(
-                    img_centerx, img_centery, size)
+                # Update max
+                contour, area = self.get_max_contour(color_mask)
+                if contour is not None and area > area_max:
+                    largest_contour = contour
+                    detected_color = i
+                    area_max = area
 
-                cv2.drawContours(self.img, [box], -1, range_rgb[detect_color], 2)
-                cv2.putText(self.img, '(' + str(world_x) + ',' + str(world_y) + ')', (min(box[0, 0], box[2, 0]), box[2, 1] - 10),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, range_rgb[detect_color], 1)  # 绘制中心点 Plotting the center point
-                # 对比上次坐标来判断是否移动 Compare the last coordinate to determine if it is moving
-                distance = math.sqrt(
-                    pow(world_x - last_x, 2) + pow(world_y - last_y, 2))
-                last_x, last_y = world_x, world_y
-                track = True
-                #print(count,distance)
-                # 累计判断 Cumulative judgment
-                if action_finish:
-                    if distance < 0.3:
-                        center_list.extend((world_x, world_y))
-                        count += 1
-                        if start_count_t1:
-                            start_count_t1 = False
-                            t1 = time.time()
-                        if time.time() - t1 > 1.5:
-                            rotation_angle = rect[2]
-                            start_count_t1 = True
-                            world_X, world_Y = np.mean(
-                                np.array(center_list).reshape(count, 2), axis=0)
-                            count = 0
-                            center_list = []
-                            start_pick_up = True
-                    else:
-                        t1 = time.time()
-                        start_count_t1 = True
-                        count = 0
-                        center_list = []
-            
-        return self.img
-    
+        return largest_contour, detected_color, area_max
+
+    def get_center(self, contour, img_size=(640, 480)):
+        """
+        Return the center of a bounding box surrounding the contour,
+        and the box itself for plotting later
+        """
+        rect = cv2.minAreaRect(contour)
+        box = np.int0(cv2.boxPoints(rect))
+
+        roi = getROI(box)  # from a * import
+        img_x, img_y = getCenter(
+            rect, roi, img_size, square_length
+        )  # also from a * import
+        world_x, world_y = convertCoordinate(
+            img_x, img_y, img_size)  # * import
+
+        return (world_x, world_y), box
+
+    def detect_once(self, img, target_colors=("red",)):
+        """
+        Find center of largest color contour
+        Return center, its area, its color, and the bounding box
+        """
+        img_lab = cv2.cv2Color(img, cv2.COLOR_BGR2LAB)
+        contour, color, area = self.get_color_contour(img_lab, target_colors)
+
+        if contour is None:
+            return None, 0, None, None
+        else:
+            center, box = self.get_center(contour)
+            return center, area, color, box
+
+    def draw_detection(self, img, center, color, box):
+        img = img.copy()
+        img_h, img_w = img.shape[:2]
+
+        # Draw cross over center of image
+        cv2.line(img, (0, int(img_h / 2)),
+                 (img_w, int(img_h / 2)), (0, 0, 200), 1)
+        cv2.line(img, (int(img_w / 2), 0),
+                 (int(img_w / 2), img_h), (0, 0, 200), 1)
+
+        if box is not None:
+            cv2.drawContours(img, [box], -1, range_rgb[color], 2)
+            cv2.putText(
+                img,
+                f"({center[0]}, {center[1]})",
+                (min(box[0, 0], box[2, 0]), box[2, 1] - 10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                range_rgb[color],
+                1,
+            )
+        return img
+
+    def tuple_distance(self, t1, t2):
+        t11, t12 = t1
+        t21, t22 = t2
+        return math.sqrt((t21 - t11) ** 2 + (t22 - t12) ** 2)
+
+    def try_detect(
+        self,
+        min_time=1.5,
+        max_distance=0.3,
+        min_area=2500,
+        timeout=10,
+        show=False,
+        size=(640, 480),
+    ):
+        """
+        Consolidate detections over multiple frames to "find" a colored box.
+        Contours of the same color must be detected repeatedly over the
+        interval of min_time, each with a center within max_distance of the previous,
+        and only contours > min_area will be counted.
+        If conditions not met by timeout seconds, throw TimeoutError.
+        Draw details to cv2.show if show is True.
+        Returns accumulated x and y on success
+        """
+        start_t = time.time()
+        detect_t = None  # time of first detection
+        centers = []
+        saved_color = None
+
+        while time.time() - start_t <= timeout:
+            img = self.camera.frame
+            if img is None:
+                continue
+
+            img = cv2.resize(img, size, interpolation=cv2.INTER_NEAREST)
+            img = cv2.GaussianBlur(img, (11, 11), 11)
+
+            # Try to detect a box
+            center, area, color, box = self.detect_once(img)
+
+            if show:
+                frame = self.draw_detection(img, center, color, box)
+                cv2.imshow("frame", frame)
+
+            if center is None or area < min_area:
+                continue
+
+            # Find distance to previous center
+            if len(centers) == 0:
+                distance = 0
+            else:
+                distance = self.tuple_distance(center, centers[-1])
+
+            # Reset if criteria not met
+            if distance > max_distance or color != saved_color:
+                centers = []
+                saved_color = None
+                detect_t = None
+                continue
+
+            # Append to centers, start detection timer if not yet
+            if detect_t is None:
+                detect_t = time.time()
+                saved_color = color
+            centers.append(center)
+
+            # If successfully detected long enough, average and return
+            if time.time() - detect_t > min_time:
+                centers = np.array(centers)
+                x, y = np.mean(centers, axis=0)
+                return (x, y), color
+
+        raise TimeoutError
 
 
+if __name__ == "__main__":
+    detector = BoxDetector()
+
+    try:
+        while True:
+            try:
+                (x, y), color = detector.try_detect()
+                print(f"Successful detection of color {color} at {x},{y}")
+            except TimeoutError:
+                print("No detection for 10s")
+    except KeyboardInterrupt:
+        print("Ending.")
+
+    cv2.destroyAllWindows()
